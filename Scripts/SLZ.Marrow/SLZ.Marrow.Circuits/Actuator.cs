c0ee8e445fa8fae8990af5d4a09649c707091dfe
doge15567 <41397214+doge15567@users.noreply.github.com>
2026-02-08 14:36:58 -0600

    add missing to actuator, actuatorsocket

--- Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/Actuator.cs
+++ Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/Actuator.cs
@@ -1,11 +1,94 @@
- 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SLZ.Marrow.Circuits;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace SLZ.Marrow.Circuits
 {
     public class Actuator : MonoBehaviour
     {
-#if UNITY_EDITOR
-#endif
+        private float _updateFixedTime = -1f;
+        private float _updateSeed;
+        private Circuit[] _inputs;
+        protected ExternalActuator _externalActuator;
+        public double updateFixedTime
+        {
+            get
+            {
+                float num = _updateFixedTime;
+            }
+        }
+        public float updateSeed
+        {
+            get
+            {
+                return _updateSeed;
+            }
+        }
+        public ExternalActuator externalActuator
+        {
+            get
+            {
+                return _externalActuator;
+            }
+            set
+            {
+                _externalActuator = value;
+            }
+        }
+        protected virtual void Awake()
+        {
+        }
+        protected virtual void Start()
+        {
+        }
+        protected virtual void OnDestroy()
+        {
+        }
+        protected virtual void Initialize()
+        {
+        }
+        public void RecursiveUpdate(float fixedTime)
+        {
+        }
+        public void InitializingUpdate(float seed)
+        {
+        }
+        protected virtual void Actuate(double fixedTime, bool isInitializing = false)
+        {
+        }
+        public virtual void SetupInputs()
+        {
+        }
+        protected void SetInputs()
+        {
+        }
+        protected void SetInputs(Circuit circuitA)
+        {
+        }
+        protected void SetInputs(Circuit circuitA, Circuit circuitB)
+        {
+        }
+        protected void SetInputs(Circuit circuitA, Circuit circuitB, Circuit circuitC)
+        {
+            
+        }
+        protected void SetInputs(Circuit[] circuits)
+        {
+        }
+        private void LinkInputs()
+        {
+        }
+        private void UnlinkInputs()
+        {
+        }
+        public Actuator()
+        {
+            int num = 0;
+            _updateSeed = -1f;
+            _inputs = new Circuit[num];
+        }
     }
 }
--- Scripts/SLZ.Marrow/SLZ/Marrow/Circuits/ActuatorSocket.cs
+++ Scripts/SLZ.Marrow/SLZ/Marrow/Circuits/ActuatorSocket.cs
@@ -1,37 +1,41 @@
-﻿using System;
+﻿using SLZ.Marrow;
+using SLZ.Marrow.Circuits;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace SLZ.Marrow.Circuits
 {
 	public class ActuatorSocket : Actuator
 	{
-		public Circuit input
-		{
-			get
-			{
-				return null;
-			}
-			set
-			{
-			}
-		}
-
-
-		protected virtual void Reset()
-		{
-		}
-
-		public ActuatorSocket()
-		{
-		}
-
-		[SerializeField]
-		private Circuit _input;
-
-		[SerializeField]
-		protected Servo _servo;
-
-		[SerializeField]
-		protected Rigidbody _rb;
-	}
+        [SerializeField]
+        private Circuit _input;
+        [SerializeField]
+        protected Servo _servo;
+        [SerializeField]
+        protected Rigidbody _rb;
+        public Circuit input
+        {
+            get
+            {
+                return _input;
+            }
+            set
+            {
+                _input = value;
+            }
+        }
+        public override void SetupInputs()
+        {
+        }
+        protected virtual void Reset()
+        {
+        }
+        public ActuatorSocket()
+        {
+            base._updateFixedTime = -1f;
+            int num = 0;
+            base._updateSeed = -1f;
+            base._inputs = new Circuit[num];
+        }
+    }
 }
